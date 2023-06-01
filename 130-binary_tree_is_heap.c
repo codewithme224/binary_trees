c0504200594ca535fc4f614a5f76ed763fde81b4
codewithme224 <codewithme224@gmail.com>
2023-06-01 13:44:09 -0700

    checks if a binary tree is a valid Max Binary Heap

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -9,18 +9,25 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t l_height = 0, r_height = 0;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
+	{
 		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-		return (left_height + 1);
+	}
 	else
-		return (right_height + 1);
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return (tree->parent != NULL);
+		if (tree)
+		{
+			l_height = tree->left ? 1 +
+				binary_tree_height(tree->left) : 0;
+			r_height = tree->right ? 1 +
+				binary_tree_height(tree->right) : 0;
+		}
+		return ((l_height > r_height) ? l_height : r_height);
+	}
 }
 
 
