ed4f6bba489570560059f3521784f54d73df0d68
codewithme224 <codewithme224@gmail.com>
2023-06-01 07:47:12 -0700

    checks if a binary tree is a valid AVL Tree

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -1,37 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * balance_tree - Balances an AVL tree.
- * @node: A pointer to the node to balance.
+ * balance_t - Balances an AVL tree.
+ * @tree: A pointer to the node to balance.
  *
  * Return: A pointer to the new root node of the tree after balancing.
  */
 
-avl_t *balance_tree(avl_t *node)
+void balance_t(avl_t **tree)
 {
-	int balance_factor;
+	int balance_f;
 
-	balance_factor = binary_tree_balance(node);
-
-	if (balance_factor > 1 && binary_tree_balance(node->left) >= 0)
-		return (binary_tree_rotate_right(node));
-
-	if (balance_factor > 1 && binary_tree_balance(node->left) < 0)
-	{
-		node->left = binary_tree_rotate_left(node->left);
-		return (binary_tree_rotate_right(node));
-	}
-
-	if (balance_factor < -1 && binary_tree_balance(node->right) <= 0)
-		return (binary_tree_rotate_left(node));
-
-	if (balance_factor < -1 && binary_tree_balance(node->right) > 0)
-	{
-		node->right = binary_tree_rotate_right(node->right);
-		return (binary_tree_rotate_left(node));
-	}
-
-	return (node);
+	if (tree == NULL || *tree == NULL)
+		return;
+	if ((*tree)->left == NULL && (*tree)->right == NULL)
+		return;
+	balance_t(&(*tree)->left);
+	balance_t(&(*tree)->right);
+	balance_f = binary_tree_balance((const binary_tree_t *)*tree);
+	if (balance_f > 1)
+		*tree = binary_tree_rotate_right((binary_tree_t *)*tree);
+	else if (balance_f < -1)
+		*tree = binary_tree_rotate_left((binary_tree_t *)*tree);
 }
 
 
@@ -118,45 +108,22 @@ int remove_type(bst_t *root)
 
 avl_t *bst_remove(avl_t *root, int value)
 {
-	avl_t *node;
+	int node_type = 0;
 
 	if (root == NULL)
 		return (NULL);
-	if (root->n > value)
-		root->left = bst_remove(root->left, value);
-	else if (root->n < value)
-		root->right = bst_remove(root->right, value);
-	else
+	if (value < root->n)
+		bst_remove(root->left, value);
+	else if (value > root->n)
+		bst_remove(root->right, value);
+	else if (value == root->n)
 	{
-		if (root->left == NULL && root->right == NULL)
-		{
-			free(root);
-			return (NULL);
-		}
-
-		else if (root->left == NULL)
-		{
-			node = root->right;
-			free(root);
-			return (node);
-		}
-
-		else if (root->right == NULL)
-		{
-			node = root->left;
-			free(root);
-			return (node);
-		}
-
-		node = root->right;
-
-		while (node->left != NULL)
-			node = node->left;
-
-		root->n = node->n;
-		root->right = bst_remove(root->right, node->n);
+		node_type = remove_type(root);
+		if (node_type != 0)
+			bst_remove(root->right, node_type);
 	}
-
+	else
+		return (NULL);
 	return (root);
 }
 
@@ -171,16 +138,10 @@ avl_t *bst_remove(avl_t *root, int value)
 
 avl_t *avl_remove(avl_t *root, int value)
 {
-	avl_t *node;
+	avl_t *root_a = (avl_t *) bst_remove((bst_t *) root, value);
 
-	if (root == NULL)
+	if (root_a == NULL)
 		return (NULL);
-	node = bst_remove(root, value);
-
-	if (node == NULL)
-		return (NULL);
-
-	root = balance_tree(node);
-
-	return (root);
+	balance_t(&root_a);
+	return (root_a);
 }
