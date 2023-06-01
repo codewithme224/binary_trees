1227c3e85ce21c65417a0bdd0c5d28fe00d9b485
codewithme224 <codewithme224@gmail.com>
2023-06-01 07:25:59 -0700

    checks if a binary tree is a valid AVL Tree

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -34,6 +34,80 @@ avl_t *balance_tree(avl_t *node)
 	return (node);
 }
 
+
+/**
+ * successor_node - get the next successor i mean the min
+ *                  node in the right subtree
+ * @node: tree to check
+ * Return: the min value of this tree
+ */
+int successor_node(bst_t *node)
+{
+	int left = 0;
+
+	if (node == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		left = successor_node(node->left);
+		if (left == 0)
+		{
+			return (node->n);
+		}
+		return (left);
+	}
+
+}
+
+/**
+ *remove_type - function that removes a node depending of its children
+ *@root: node to remove
+ *Return: 0 if it has no children or other value if it has
+ */
+int remove_type(bst_t *root)
+{
+	int new_value = 0;
+
+	if (!root->left && !root->right)
+	{
+		if (root->parent->right == root)
+			root->parent->right = NULL;
+		else
+			root->parent->left = NULL;
+		free(root);
+		return (0);
+	}
+	else if ((!root->left && root->right) || (!root->right && root->left))
+	{
+		if (!root->left)
+		{
+			if (root->parent->right == root)
+				root->parent->right = root->right;
+			else
+				root->parent->left = root->right;
+			root->right->parent = root->parent;
+		}
+		if (!root->right)
+		{
+			if (root->parent->right == root)
+				root->parent->right = root->left;
+			else
+				root->parent->left = root->left;
+			root->left->parent = root->parent;
+		}
+		free(root);
+		return (0);
+	}
+	else
+	{
+		new_value = successor_node(root->right);
+		root->n = new_value;
+		return (new_value);
+	}
+}
+
 /**
  * bst_remove - Removes a node from a Binary Search Tree.
  * @root: A pointer to the root node of the BST.
@@ -92,8 +166,7 @@ avl_t *bst_remove(avl_t *root, int value)
  * @root: A pointer to the root node of the tree for removing a node.
  * @value: The value to remove in the tree.
  *
- * Return: A pointer to the new root node of the tree after
- *         removing the desired value, and after rebalancing.
+ * Return: removing the desired value, and after rebalancing.
  */
 
 avl_t *avl_remove(avl_t *root, int value)
